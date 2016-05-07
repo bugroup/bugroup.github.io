4ba6ba9be08cca537c794470a8b63750c2ed63cf
MoViS08 <d08121992@mail.ru>
2016-05-07 15:12:40 +0500

    123

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div id = "footer">
+	<div class = "container">
+	<p style="text-align: center;">созданно в <a href="http://vgit.ru">vgit.ru</a> и <a href="http://mpc-first.ru">mpc-first.ru</a></p>
+	</div>
+</div>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+</body>
+</html>
